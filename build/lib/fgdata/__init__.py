1bd01ef2fd22303280f9d1962c9ba0575e03331d
shuzip <shuzip@shuzipdeMacBook-Pro.local>
2025-01-05 15:13:44 +0800

    fgdata create

--- build/lib/fgdata/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from .futures.future_news import futures_news_sina
-
-# from .other_module import some_function
--- build/lib/fgdata/futures/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# from .future_news import futures_news_sina
